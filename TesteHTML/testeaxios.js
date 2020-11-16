7a483d2815f7b86dfb92453193e65a51e0dbc1dc
Bernardo Viseu <Pacifico53@users.noreply.github.com>
2020-11-16 14:59:09 +0000

    get a funcionar

---
+++ TesteHTML/testeaxios.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const http = require('http');
+               
+var servidor = http.createServer(function (req, res) {
+    const options = {
+        headers: { 'X-AIO-Key': 'aio_nqoI77kQJQHwz74qcy5vhBwm0zOV' }
+    };
+
+    axios.get('https://io.adafruit.com/api/v2/Paces53/feeds/sensorfeed/data/last', options)
+                .then((resp) => {
+                    res.writeHead(200, {
+                        'Content-type': 'text/html; charset=utf-8'
+                    })
+                    c = resp;
+                    res.write('<p>??????'+ c + '???????</p>');
+                    console.log(c);
+                    res.end();
+                }).catch((err) => {
+                    console.log('Erro :'+err);
+                    res.write('<p>Pedido nao suportado: ' + req.method + '</p>');
+                    res.end();
+               });
+                }).listen(7777)
+            
+            console.log('Listening on 7777...')
