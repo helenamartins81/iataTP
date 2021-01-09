49fd331138167dba059c49319298e3b81811d18f
Pacifico53 <bernardo2viseu@gmail.com>
2021-01-09 17:22:21 +0000

    yo looking good bro nice chat with suggestions

--- TP2/pychatbot/run.py
+++ TP2/pychatbot/run.py
@@ -1,12 +1,9 @@
 import aiml
 import os
-import random
 from autocorrect import Speller
-
 spell = Speller()
 
 BRAIN_FILE = "brain.brn"
-sessionId = random.randrange(10000)
 
 kernel = aiml.Kernel()
 
@@ -15,20 +12,17 @@ if os.path.exists(BRAIN_FILE):
     kernel.loadBrain(BRAIN_FILE)
 else:
     print("Parsing aiml files")
-    kernel.bootstrap(learnFiles="learningFileList.aiml", commands="load aiml")
+    kernel.bootstrap(learnFiles="aiml/learningFileList.aiml", commands="load aiml")
     print("Saving brain file: " + BRAIN_FILE)
     kernel.saveBrain(BRAIN_FILE)
-
-print("Welcome to PyChatBot!")
 
 # Press CTRL-C to break this loop
 while True:
     msg = input(">>> ")
     msg = [spell(w) for w in (msg.split())]
     question = " ".join(msg)
-    response = kernel.respond(question, sessionId)
+    response = kernel.respond(question)
     if response:
         print("PyChatBot > ", response)
     else:
         print("PyChatBot > Sorry, I don't understand...", )
-
